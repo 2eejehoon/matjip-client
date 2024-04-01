d6929ac4146ec49593561393397fb770b1f4e8ab
이제훈 <2eejehoon@gmail.com>
2024-04-01 15:36:09 +0900

    find-id, find-password 페이지 추가

---
+++ src/pages/users/find-id.tsx
@@ -0,0 +1,12 @@
+import BaseLayout from "@/layouts/baseLayout";
+import { ReactElement } from "react";
+
+const FindIdPage = () => {
+    return <></>;
+};
+
+FindIdPage.getLayout = (page: ReactElement) => {
+    return <BaseLayout>{page}</BaseLayout>;
+};
+
+export default FindIdPage;
---
+++ src/pages/users/find-password.tsx
@@ -0,0 +1,12 @@
+import BaseLayout from "@/layouts/baseLayout";
+import { ReactElement } from "react";
+
+const FindPasswordPage = () => {
+    return <></>;
+};
+
+FindPasswordPage.getLayout = (page: ReactElement) => {
+    return <BaseLayout>{page}</BaseLayout>;
+};
+
+export default FindPasswordPage;
