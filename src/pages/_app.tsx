5b9e2aaead4750ea206e207997a6ab8f3af841c0
이제훈 <2eejehoon@gmail.com>
2024-04-02 16:46:18 +0900

    react-query provider 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,26 @@ import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import GlobalStyle from "@/styles/globalStyle";
 import StyledComponentsThemeProvider from "@/styles/themeProvider";
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useState } from "react";
 import { BottomSheetProvider } from "@/components/bottomSheet";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 type MatjipProps = AppProps & {
     Component: NextPage & { getLayout: (page: ReactElement) => ReactNode };
 };
 
 export default function App({ Component, pageProps }: MatjipProps) {
+    const [queryClient] = useState(() => new QueryClient({}));
     const getLayout = Component.getLayout ?? ((page) => page);
 
     return (
         <StyledComponentsThemeProvider>
             <GlobalStyle />
-            <BottomSheetProvider>
-                {getLayout(<Component {...pageProps} />)}
-            </BottomSheetProvider>
+            <QueryClientProvider client={queryClient}>
+                <BottomSheetProvider>
+                    {getLayout(<Component {...pageProps} />)}
+                </BottomSheetProvider>
+            </QueryClientProvider>
         </StyledComponentsThemeProvider>
     );
 }
