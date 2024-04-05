0b8ad761b6be507a99d257d23437c58243b2cb85
이제훈 <2eejehoon@gmail.com>
2024-04-05 21:44:25 +0900

    헤더 검색창

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,21 @@
 import Link from "next/link";
 import styled from "styled-components";
-import { useBottomSheetContext } from "./bottomSheet";
-import { CiUser, CiHome } from "react-icons/ci";
+import { CiUser, CiHome, CiSearch } from "react-icons/ci";
 import { ROUTE_MAP } from "@/utils/route";
 
 const Header = () => {
-    const { setContentToBottomSheet, openBottomSheet } = useBottomSheetContext();
-
     return (
         <Wrapper>
             <Link href={ROUTE_MAP["HOME"]}>
                 <Home />
             </Link>
-            <Rightbar>
-                <Link href={ROUTE_MAP["PROFILE"]}>
-                    <Profile />
-                </Link>
-            </Rightbar>
+            <SearchContainer>
+                <Search />
+                <SearchInput placeholder="맛집을 검색해보세요." />
+            </SearchContainer>
+            <Link href={ROUTE_MAP["PROFILE"]}>
+                <Profile />
+            </Link>
         </Wrapper>
     );
 };
@@ -41,9 +40,36 @@ const Home = styled(CiHome)`
     }
 `;
 
-const Rightbar = styled.div`
+const SearchContainer = styled.div`
     display: flex;
-    gap: 12px;
+    gap: 2px;
+    width: 50%;
+    min-width: 200px;
+    padding: 8px;
+    border: ${({ theme }) => theme.border.thin};
+    border-radius: ${({ theme }) => theme.borderRadius.large};
+    box-shadow:
+        0 2px 4px rgba(0, 0, 0, 0.19),
+        0 1px 1px rgba(0, 0, 0, 0.23);
+
+    &:focus-within {
+        border-color: black;
+    }
+`;
+
+const Search = styled(CiSearch)`
+    width: ${({ theme }) => theme.element.icon.width};
+    height: ${({ theme }) => theme.element.icon.height};
+`;
+
+const SearchInput = styled.input`
+    width: 100%;
+    padding: 4px;
+    outline: none;
+    border: none;
+    color: ${({ theme }) => theme.color.black};
+    font-size: ${({ theme }) => theme.fontSize.small};
+    font-weight: ${({ theme }) => theme.fontWeight.normal};
 `;
 
 const Profile = styled(CiUser)`
