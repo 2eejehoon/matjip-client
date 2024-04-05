b71ed1512952b756bbe8e05d4912d092ca7bd11e
이제훈 <2eejehoon@gmail.com>
2024-04-05 23:20:06 +0900

    유저 정보 받아오기

--- src/pages/users/profile.tsx
+++ src/pages/users/profile.tsx
@@ -1,7 +1,31 @@
 import BaseLayout from "@/layouts/baseLayout";
+import { useQuery } from "@tanstack/react-query";
 import { ReactElement } from "react";
+import Cookies from "js-cookie";
 
 const UserProfilePage = () => {
+    const { data: user } = useQuery({
+        queryKey: ["USER"],
+        queryFn: async () => {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/users/profile`, {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                    Authorization: `Bearer ${Cookies.get("accessToken")}`,
+                    "Content-Type": "application/json"
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error();
+            }
+
+            return response.json();
+        }
+    });
+
+    console.log(user);
+
     return <></>;
 };
 
