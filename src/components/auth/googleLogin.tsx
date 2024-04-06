3f761a7d79f0b11ad938a70e4b244d79c8c4f37f
이제훈 <2eejehoon@gmail.com>
2024-04-06 10:18:42 +0900

    원복

--- src/components/auth/googleLogin.tsx
+++ src/components/auth/googleLogin.tsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 import { RiGoogleFill } from "react-icons/ri";
-import { ROUTE_MAP } from "@/utils/route";
 
 const GoogleLogin = () => {
     const onGoogleLogin = () => {
-        window.location.replace(`${process.env.NEXT_PUBLIC_BASE_API_URL}${ROUTE_MAP["GOOGLE_LGOIN"]}`);
+        window.location.replace(`${process.env.NEXT_PUBLIC_BASE_API_URL}/auth/google`);
     };
 
     return <Google onClick={onGoogleLogin} />;
