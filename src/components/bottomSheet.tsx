f2074fe54475de57d9a4d94d71a29554cdb08a57
이제훈 <2eejehoon@gmail.com>
2024-04-16 22:38:26 +0900

    바텀시트 드래그 높이 조절 기능 추가

--- src/components/bottomSheet.tsx
+++ src/components/bottomSheet.tsx
@@ -1,103 +1,143 @@
-import { PropsWithChildren, ReactNode, createContext, useContext, useState } from "react";
+import { PropsWithChildren, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { motion, useAnimation } from "framer-motion";
-import Portal from "./portal";
 
-export const BottomSheetContext = createContext({
-    isBottomSheetOpen: false,
-    openBottomSheet: () => {},
-    closeBottomSheet: () => {},
-    animate: {}
-});
+const MIN_HEIGHT = 300;
+const MAX_HEIGHT = 700;
+const TEMP = 30;
 
-export const BottomSheetProvider = ({ children }: PropsWithChildren) => {
-    const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
+type BottomSheetProps = PropsWithChildren & {
+    isOpen: boolean;
+};
+
+const BottomSheet = ({ children, isOpen }: BottomSheetProps) => {
     const animate = useAnimation();
+    const bottomSheetRef = useRef<HTMLDivElement>(null);
+    const touchRef = useRef({
+        startY: 0,
+        endY: 0
+    });
+    const bottomSheetStateRef = useRef<"hidden" | "visible" | "expanded">("hidden");
 
-    const openBottomSheet = () => {
-        animate.start("visible");
-        setIsBottomSheetOpen(true);
-    };
+    useEffect(() => {
+        if (isOpen) {
+            animate.start("visible");
+            bottomSheetStateRef.current = "visible";
+            document.body.style.overflow = "hidden";
+        } else {
+            animate.start("hidden");
+            bottomSheetStateRef.current = "hidden";
+            document.body.style.overflow = "auto";
+        }
+    }, [isOpen, animate]);
 
-    const closeBottomSheet = () => {
-        animate.start("hidden");
-        setIsBottomSheetOpen(false);
-    };
+    useEffect(() => {
+        if (!bottomSheetRef.current) {
+            return;
+        }
+
+        const bottomSheetDiv = bottomSheetRef.current;
+
+        const onTouchMove = (e: TouchEvent) => {
+            const newHeight = window.innerHeight - e.touches[0].clientY;
+            if (newHeight < MIN_HEIGHT) {
+                bottomSheetDiv.style.height = `${MIN_HEIGHT}px`;
+                return;
+            }
+
+            if (newHeight > MAX_HEIGHT) {
+                bottomSheetDiv.style.height = `${MAX_HEIGHT}px`;
+                return;
+            }
+
+            bottomSheetDiv.style.height = `${newHeight}px`;
+        };
+
+        const onTouchStart = (e: TouchEvent) => {
+            touchRef.current.startY = e.touches[0].clientY;
+            bottomSheetDiv.style.removeProperty("transition");
+        };
+
+        const onTouchEnd = (e: TouchEvent) => {
+            touchRef.current.endY = e.changedTouches[0].clientY;
+            bottomSheetDiv.style.setProperty("transition", "height 0.2s linear");
+
+            if (touchRef.current.startY - touchRef.current.endY > TEMP) {
+                bottomSheetStateRef.current = "expanded";
+                bottomSheetDiv.style.height = `${MAX_HEIGHT}px`;
+                return;
+            }
+
+            if (touchRef.current.endY - touchRef.current.startY > TEMP) {
+                if (bottomSheetStateRef.current === "visible") {
+                    bottomSheetStateRef.current = "hidden";
+                    animate.start("hidden");
+                } else if (bottomSheetStateRef.current === "expanded") {
+                    bottomSheetStateRef.current = "visible";
+                    bottomSheetDiv.style.height = `${MIN_HEIGHT}px`;
+                }
+                return;
+            }
+
+            touchRef.current.startY = 0;
+            touchRef.current.endY = 0;
+        };
+
+        bottomSheetDiv.addEventListener("touchmove", onTouchMove);
+        bottomSheetDiv.addEventListener("touchstart", onTouchStart);
+        bottomSheetDiv.addEventListener("touchend", onTouchEnd);
+        return () => {
+            bottomSheetDiv.removeEventListener("touchmove", onTouchMove);
+            bottomSheetDiv.removeEventListener("touchstart", onTouchStart);
+            bottomSheetDiv.removeEventListener("touchend", onTouchEnd);
+        };
+    }, []);
 
     return (
-        <BottomSheetContext.Provider
-            value={{
-                isBottomSheetOpen,
-                openBottomSheet,
-                closeBottomSheet,
-                animate
-            }}
+        <_BottomSheet
+            ref={bottomSheetRef}
+            initial="hidden"
+            animate={animate}
+            variants={{ visible: { y: "0" }, hidden: { y: "100%" } }}
+            transition={{ type: "spring", damping: 50, stiffness: 500 }}
         >
+            <Header />
             {children}
-        </BottomSheetContext.Provider>
+        </_BottomSheet>
     );
 };
 
-export const useBottomSheetContext = () => {
-    return useContext(BottomSheetContext);
-};
-
-type BottomSheetProps = PropsWithChildren & {};
-
-const BottomSheet = ({ children }: BottomSheetProps) => {
-    const { isBottomSheetOpen, animate, closeBottomSheet } = useBottomSheetContext();
-
-    return (
-        <>
-            <Portal>
-                <Wrapper
-                    initial="hidden"
-                    animate={animate}
-                    variants={{ visible: { y: "0" }, hidden: { y: "100%" } }}
-                    transition={{ type: "spring", damping: 50, stiffness: 500 }}
-                >
-                    <Handle />
-                    {children}
-                </Wrapper>
-            </Portal>
-            {isBottomSheetOpen && <Overlay onClick={closeBottomSheet} />}
-        </>
-    );
-};
-
-const Wrapper = styled(motion.div)`
+const _BottomSheet = styled(motion.div)`
     display: flex;
     flex-direction: column;
     position: fixed;
     bottom: 0;
-    border-radius: 12px 12px 0 0;
-    padding: 0 12px 0 12px;
+    border-radius: 16px 16px 0 0;
     width: 100%;
-    height: auto;
-    min-height: 50%;
-    z-index: 1;
+    height: 300px;
     border: ${({ theme }) => theme.border.thin};
     background-color: ${({ theme }) => theme.backgroundColor.white};
 `;
 
-const Overlay = styled.div`
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
+const Header = () => {
+    return (
+        <_Header>
+            <Handle />
+        </_Header>
+    );
+};
+
+const _Header = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 0 8px 0;
 `;
 
 const Handle = styled.div`
-    width: 15%;
-    height: 5px;
-    left: 0;
-    right: 0;
-    margin-top: 12px;
-    margin-bottom: 12px;
-    margin-left: auto;
-    margin-right: auto;
+    width: 20%;
+    height: 4px;
+    border-radius: 4px;
     background-color: lightgray;
 `;
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,53 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import BaseLayout from "@/layouts/baseLayout";
+import BottomSheet from "@/components/bottomSheet";
 
 const HomePage = () => {
-    return <></>;
+    const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
+
+    return (
+        <>
+            <button onClick={() => setIsBottomSheetOpen((prev) => !prev)}>123</button>
+            <BottomSheet isOpen={isBottomSheetOpen}>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+                <div>123</div>
+            </BottomSheet>
+        </>
+    );
 };
 
 HomePage.getLayout = (page: ReactElement) => {
