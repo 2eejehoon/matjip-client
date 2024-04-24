86cb254958a8f74144839c25780457a690d3d5ed
이제훈 <2eejehoon@gmail.com>
2024-04-24 22:47:40 +0900

    name, passwordConfirm 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -10,8 +10,8 @@ export const login = async ({ email, password }: LoginDto): Promise<Token> => {
     return data;
 };
 
-export const signup = async ({ email, password }: SignupDto): Promise<Token> => {
-    const { data } = await Axios.post<Token>("/auth/signup", { email, password });
+export const signup = async ({ email, name, password, passwordConfirm }: SignupDto): Promise<Token> => {
+    const { data } = await Axios.post<Token>("/auth/signup", { email, name, password, passwordConfirm });
 
     return data;
 };
